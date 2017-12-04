5dc807680370006baebbd6a06744c297e711cc25
eddu_lkcr <eddu_lkcr@outlook.es>
2017-12-04 11:48:32 -0500

    style

--- 03PatronMVC/views/template.php
+++ 03PatronMVC/views/template.php
@@ -5,6 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Template</title>
+    <style>
+
+		header{
+			position:relative;
+			margin:auto;
+			text-align:center;
+			padding:5px;	
+		}
+		
+		nav{
+			position:relative;
+			margin:auto;
+			width:100%;
+			height:auto;
+			background:black;
+		}
+
+		nav ul{
+			position:relative;
+			margin:auto;
+			width:50%;
+			text-align: center;
+		}
+
+		nav ul li{
+			display:inline-block;
+			width:24%;
+			line-height: 50px;
+			list-style: none;
+		}
+
+		nav ul li a{
+			color:white;
+			text-decoration: none;
+		}
+
+		section{
+			position:relative;
+			padding:20px;
+		}
+	
+	</style>
 </head>
 <body>
     
